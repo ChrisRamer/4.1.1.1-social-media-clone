422759b380ddd8f45e2302c5456418798e2d0fdd
Chris Ramer <chrisramer.97@gmail.com>
2021-09-20 11:07:08 -0700

    I really should be committing more...

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import StatusSection from "./StatusSection";
+
+function App() {
+	return (
+		<React.Fragment>
+			<Header />
+			<StatusSection />
+		</React.Fragment>
+	);
+}
+
+export default App;
---
+++ src/components/StatusSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function StatusSection() {
+	return (
+		<div id="statusSection" class="panel">
+			<h3>Talk about your day!</h3>
+			<form id="statusInput">
+				<div class="form-group">
+					<input id="statusText" class="form-control" type="text"></input>
+				</div>
+			</form>
+		</div>
+	);
+}
+
+export default StatusSection;
