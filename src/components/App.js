674203b882dd05476d9502c70c3104361989425e
Chris Ramer <chrisramer.97@gmail.com>
2021-09-21 10:36:41 -0700

    Add summary section

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import StatusSection from "./StatusSection";
+import SummarySection from "./SummarySection";
 
 function App() {
 	return (
 		<React.Fragment>
 			<Header />
 			<StatusSection />
+			<SummarySection />
 		</React.Fragment>
 	);
 }
---
+++ src/components/SummarySection.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function StatusSection() {
+	return (
+		<div id="summarySection" class="panel">
+			<p>Hello, world!</p>
+		</div>
+	);
+}
+
+export default StatusSection;
