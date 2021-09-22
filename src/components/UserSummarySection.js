7763efe0ec7528e77ad34613dca184261650d7e6
Chris Ramer <chrisramer.97@gmail.com>
2021-09-22 11:37:22 -0700

    Add image to user summary

--- src/components/UserSummarySection.js
+++ src/components/UserSummarySection.js
@@ -1,8 +1,10 @@
 import React from "react";
+import userImage from "./../img/user-icon.png";
 
 function UserSummarySection() {
 	return (
 		<div id="userSummarySection" class="panel">
+			<img src={userImage} id="userImage" alt="User avatar" align="left" />
 			<p>Hello, world!</p>
 			<p>This is the user summary section</p>
 		</div>
