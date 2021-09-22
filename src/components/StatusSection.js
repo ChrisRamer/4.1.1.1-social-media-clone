86ac9384ce536674275b3a7e4c8295e0f47fa225
Chris Ramer <chrisramer.97@gmail.com>
2021-09-22 10:48:13 -0700

    Add placeholder

--- src/components/StatusSection.js
+++ src/components/StatusSection.js
@@ -6,7 +6,7 @@ function StatusSection() {
 			<h3>Talk about your day!</h3>
 			<form id="statusInput">
 				<div class="form-group">
-					<input id="statusText" class="form-control" type="text"></input>
+					<input id="statusText" class="form-control" type="text" placeholder="What's on your mind?"></input>
 				</div>
 			</form>
 		</div>
