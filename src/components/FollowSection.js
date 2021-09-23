d677645565ebecdeb2d7c6e0ba81b35d6007adf1
Chris Ramer <chrisramer.97@gmail.com>
2021-09-23 09:20:14 -0700

    Build follow section

--- src/components/FollowSection.js
+++ src/components/FollowSection.js
@@ -1,10 +1,17 @@
 import React from "react";
+import userImage from "./../img/user-icon.png";
 
 function FollowSection() {
 	return (
 		<div id="followSection" class="panel">
-			<p>Hello, world!</p>
-			<p>This is the follow section</p>
+			<img src={userImage} id="userImage" alt="User avatar" align="left" />
+			<p>Person to follow #1</p>
+			<br />
+			<img src={userImage} id="userImage" alt="User avatar" align="left" />
+			<p>Person to follow #2</p>
+			<br />
+			<img src={userImage} id="userImage" alt="User avatar" align="left" />
+			<p>Person to follow #3</p>
 		</div>
 	);
 }
