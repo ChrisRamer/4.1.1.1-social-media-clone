3bc3ea2e8c499e9891fbe23692fc33f669b957db
Chris Ramer <chrisramer.97@gmail.com>
2021-09-23 10:19:06 -0700

    idk what to do here

--- src/components/FeedSection.js
+++ src/components/FeedSection.js
@@ -1,10 +1,12 @@
 import React from "react";
+import userImage from "./../img/user-icon.png";
 
 function FeedSection() {
 	return (
 		<div id="feedSection" class="panel">
+			<img src={userImage} id="userImage" alt="User avatar" align="left" />
 			<p>Hello, world!</p>
-			<p>This is the feed section</p>
+			<p>This is a user's tweet!</p>
 		</div>
 	);
 }
